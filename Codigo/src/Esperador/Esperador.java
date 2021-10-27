5eb89db6a87126ccf14bec05d78e99c65e03194e
Juan Dingevan <jidingevan@gmail.com>
2021-10-27 11:37:51 -0300

    Creación de los Esperadores.

--- Codigo/src/Esperador/Esperador.java
+++ Codigo/src/Esperador/Esperador.java
@@ -1,5 +1,25 @@
 package Esperador;
 
-public class Esperador {
+import Grilla.Grilla;
 
+public abstract class Esperador extends Thread {
+	protected Grilla miGrilla;
+	protected int tiempoEsperado;
+	
+	public Esperador(int t, Grilla g) {
+		tiempoEsperado = t;
+		miGrilla = g;
+	}
+	
+	public void run() {
+		try {
+			Thread.sleep(tiempoEsperado);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		notificar();
+	}
+	
+	protected abstract void notificar();
 }
