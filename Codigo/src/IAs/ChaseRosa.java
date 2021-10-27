8bd4ba3a9411fb7d09b4f3417ed2762a8c6f5d14
Juan Dingevan <jidingevan@gmail.com>
2021-10-27 13:02:29 -0300

    Creación de las "Inteligencias Artificiales" de persecución de los Fantasmas.

---
+++ Codigo/src/IAs/ChaseRosa.java
@@ -0,0 +1,13 @@
+package IAs;
+
+import Utilidad.Position;
+
+public class ChaseRosa extends ChaseIA {
+
+	@Override
+	public Position calcularSiguientePosicion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
