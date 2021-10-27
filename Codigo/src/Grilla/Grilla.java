436d00fea3775f7e9a2cb0f883368570fe3a7fb3
Juan Dingevan <jidingevan@gmail.com>
2021-10-27 11:38:00 -0300

    Actualización de la Grilla.

--- Codigo/src/Grilla/Grilla.java
+++ Codigo/src/Grilla/Grilla.java
@@ -1,5 +1,31 @@
 package Grilla;
 
-public class Grilla {
+import Utilidad.Position;
 
+public class Grilla {
+	
+	
+	public void pedirSumarPuntos(int p) {
+		
+	}
+	
+	public void decrementarMonedas() {
+		
+	}
+	
+	public void ponerFantasmasEnRun() {
+		
+	}
+
+	public void ponerBombaEn(Position p) {
+		
+	}
+	
+	public void detonarBombaEn(Position p) {
+		
+	}
+
+	public void pedirActualizarVelocidad(int v) {
+		
+	}
 }
